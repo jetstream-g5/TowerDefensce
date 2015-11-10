efbdfb34cd4491861255a8769b111df3cb69cc13
18048 <18048@ma-web.nl>
2015-11-10 08:54:31 +0100

    Clicking turret spot now changes prefabs from empty plot to turret.

---
+++ Assets/Scripts/BuildPlatform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+	public class BuildPlatform : MonoBehaviour {
+
+
+	[SerializeField] private GameObject Theonator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		Debug.Log("Hey macarena");
+		Instantiate(Theonator, this.transform.position, this.transform.rotation);
+		Destroy(this.gameObject);
+	}
+
+
+}
