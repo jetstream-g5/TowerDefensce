d55fa9fcbdc2346f083aa2522adfc4aad967cb19
Lorenzo <18927@ma-web.nl>
2015-11-16 21:18:54 +0100

    ayy lmao, healthbar, ripperino enemies
    :hammer:

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,21 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyHealth : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	// Use this for initialization
+	HealthBar healthBar;
+	//[SerializeField]private Transform healthBarObject;
+	void Start () {
+		healthBar = GetComponentInChildren<HealthBar> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag == "Bullet") {
-			//Debug.Log("hittered");
+			healthBar.LowerHealth();
+			Destroy(other.gameObject);
 		}
+	}
+
+	public void RipEnemy(){
+		Destroy (this.gameObject);
 	}
 }
